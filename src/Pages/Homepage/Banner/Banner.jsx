bb4b0b99c02c54f685737b69ce463362e20ce2cb
afifa <2023afifa@gmail.com>
2024-04-03 21:14:17 +0600

    Navbar section done

--- src/Pages/Homepage/Banner/Banner.jsx
+++ src/Pages/Homepage/Banner/Banner.jsx
@@ -1,7 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Banner = () => {
+    const [profile, setProfile] = useState({});
+
+    useEffect(() => {
+        fetch("https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae")
+            .then(res => res.json())
+            .then(data => setProfile(data))
+            .catch(error => console.error("Error: ", error))
+    }, [])
+
+
+    console.log(profile);
+
+
     return (
         <div>
-            
+            <div className="hero bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={profile.user.about.avatar.url} className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">{profile.user.about.name}</h1>
+                        <p className="py-6">{profile.user.about.subTitle}</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Homepage/Home/Home.jsx
+++ src/Pages/Homepage/Home/Home.jsx
@@ -1,7 +1,25 @@
+import About from "../About/About";
+import Banner from "../Banner/Banner";
+import Navbar from "../Navbar/Navbar";
+import Services from "../Services/Services";
+import Skills from "../Skills/Skills";
+import Projects from "../Projects/Projects";
+import Timeline from "../Timeline/Timeline";
+import Testimonial from "../Testimonial/Testimonial";
+import Contact from "../Contact/Contact";
+
 const Home = () => {
     return (
         <div>
-            <h2>Home page</h2>
+            <Navbar></Navbar>
+            <Banner></Banner>
+            <About></About>
+            <Services></Services>
+            <Skills></Skills>
+            <Projects></Projects>
+            <Timeline></Timeline>
+            <Testimonial></Testimonial>
+            <Contact></Contact>
         </div>
     );
 };
