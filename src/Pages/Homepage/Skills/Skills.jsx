b71b553906645c6371f3fc90779e9d5c52f34607
afifa <2023afifa@gmail.com>
2024-04-05 05:30:10 +0600

    Skills section done

--- src/Pages/Homepage/Skills/Skills.jsx
+++ src/Pages/Homepage/Skills/Skills.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import Skill from "./Skill";
+
 const Skills = () => {
+    const [profile, setProfile] = useState({});
+
+    useEffect(() => {
+        fetch("https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae")
+            .then(res => res.json())
+            .then(data => setProfile(data))
+            .catch(error => console.error("Error: ", error))
+    }, [])
+
     return (
         <div>
-            
+            <h2 className="text-center font-bold text-2xl mt-10 mb-5">Skills</h2>
+            <div className="grid lg:grid-cols-3 gap-10">
+                {
+                    profile.user?.skills.map(s => <Skill key={s.id} s={s}></Skill>)
+                }
+            </div>
         </div>
     );
 };
