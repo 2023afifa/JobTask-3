c797d9ebbb398f904ca92de5599702c6c9811ef6
afifa <2023afifa@gmail.com>
2024-04-05 05:37:50 +0600

    Skill section created for each card

--- src/Pages/Homepage/Skills/Skill.jsx
+++ src/Pages/Homepage/Skills/Skill.jsx
@@ -1,7 +1,21 @@
-const Skill = () => {
+const Skill = ({ s }) => {
+    const { name, sequence, percentage, image, enabled } = s;
+
     return (
         <div>
-            
+            {
+                enabled ?
+                    <div className="card w-96 bg-base-100 shadow-xl mx-auto">
+                        <figure><img src={image?.url} alt="" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{name}</h2>
+                            <p>{sequence}</p>
+                            <p>{percentage}</p>
+                        </div>
+                    </div>
+                    :
+                    <></>
+            }
         </div>
     );
 };
