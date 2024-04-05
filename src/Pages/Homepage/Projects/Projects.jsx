82859086d826f807a7f2545dd864744391f07bcd
afifa <2023afifa@gmail.com>
2024-04-05 06:01:05 +0600

    Projects section created

--- src/Pages/Homepage/Projects/Projects.jsx
+++ src/Pages/Homepage/Projects/Projects.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import Project from "./Project";
+
 const Projects = () => {
+    const [profile, setProfile] = useState({});
+
+    useEffect(() => {
+        fetch("https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae")
+            .then(res => res.json())
+            .then(data => setProfile(data))
+            .catch(error => console.error("Error: ", error))
+    }, [])
+
     return (
         <div>
-            
+            <h2 className="text-center font-bold text-2xl mt-10 mb-5">Projects</h2>
+            <div className="grid lg:grid-cols-3 gap-10">
+                {
+                    profile.user?.projects.map(p => <Project key={p.id} p={p}></Project>)
+                }
+            </div>
         </div>
     );
 };
