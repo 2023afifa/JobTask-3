df1a499e9b53209144b65e444f7171c764ca6751
afifa <2023afifa@gmail.com>
2024-04-05 12:35:53 +0600

    Testimonial review section done

--- src/Pages/Homepage/Testimonials/Testimonial.jsx
+++ src/Pages/Homepage/Testimonials/Testimonial.jsx
@@ -1,7 +1,21 @@
-const Testimonial = () => {
+const Testimonial = ({ t }) => {
+    const { name, image, position, review, enabled } = t;
+
     return (
         <div>
-            
+            {
+                enabled ?
+                    <div className="border-black border-2 p-10 mb-10 lg:flex justify-between items-center gap-10">
+                        <img className="w-40" src={image?.url} alt="" />
+                        <div>
+                            <h2>{name}</h2>
+                            <p>{position}</p>
+                            <p>{review}</p>
+                        </div>
+                    </div>
+                    :
+                    <> </>
+            }
         </div>
     );
 };
