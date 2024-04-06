d2772d767da4f03b4a09a2a6fb6ea462c1401a1f
afifa <2023afifa@gmail.com>
2024-04-06 19:57:17 +0600

    Navbar animation done

--- src/Pages/Homepage/Navbar/Navbar.jsx
+++ src/Pages/Homepage/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                     <a className="btn btn-ghost text-3xl">John Doe</a>
                 </div>
                 <div className="navbar-end">
-                    <div className="dropdown" onBlur={closeNavbar}>
+                    <div onBlur={closeNavbar}>
                         <div tabIndex={0} role="button" className="btn btn-ghost" onClick={toggleNavbar}>
                             {
                                 isOpen ?
@@ -36,11 +36,14 @@ const Navbar = () => {
                                 isOpen && (
                                     <motion.div
                                         className="navbar-menu"
-                                        initial={{ x: '100%' }}
-                                        animate={{ x: 0 }}
-                                        exit={{ x: '100%' }}
+                                        initial={{ x: '100%', opacity: 0, }}
+                                        animate={{ x: 0, opacity: 1, }}
+                                        exit={{ opacity: 0, x: '100%' }}
+                                        transition={{ duration: 0.3, type: 'tween' }}
                                     >
-                                        <ul className="menu menu-horizontal px-1">
+                                        <h2 className="text-3xl">Menu</h2>
+                                        <hr className="mb-6" />
+                                        <ul>
                                             <li><a>About</a></li>
                                             <li><a>Services</a></li>
                                             <li><a>Skills</a></li>
